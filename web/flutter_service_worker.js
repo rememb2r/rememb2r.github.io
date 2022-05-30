1f886479d29f0168a72d4f7633deb9ff497df8e6
Jonghwan <jonghwan.zhu@gmail.com>
2022-05-30 13:27:09 +0900

    pc version html

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -97,11 +97,11 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "7c08130e63cc0f1cb4a6e1590f042e84",
 "icons/ms-icon-310x310.png": "4feaa7c4ace77f601b4298dc4c61913c",
 "icons/ms-icon-70x70.png": "b8a4cb32d0e07700bcb09e5312d32a8e",
-"index.html": "f5374cb9c1335625de88de568fe8c53d",
-"/": "f5374cb9c1335625de88de568fe8c53d",
-"main.dart.js": "e497d56c325c3c1b7cedfb9fb0585ef9",
+"index.html": "ea964536ecb8754f2f3f463f57fbb264",
+"/": "ea964536ecb8754f2f3f463f57fbb264",
+"main.dart.js": "154a6933fc53e95d46824a5d1472022d",
 "manifest.json": "fc5a18716876fc4eed0f19a8ed6cb91b",
-"version.json": "46248323cc7049860bf8208e4b2f18d0"
+"version.json": "abb158a3fbd0bbd5335dcdee462b9964"
 };
 
 // The application shell files that are downloaded before a service worker can
